66b3ea92ee6cc050bdd88be5e915d9d7b5f33d51
M17 <abisheknimalan@gmail.com>
2023-03-24 11:42:13 +0530

    blah

---
+++ Source/horse/horse.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "horse.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, horse, "horse" );
